27a99ea218d63740c7b3934898b3f3ff6ccb5aec
Carlos Santana <csantanapr@apache.org>
2018-05-10 18:41:09 -0400

    update dockerfile openjdk

--- tools/travis/test.sh
+++ tools/travis/test.sh
@@ -5,13 +5,7 @@ set -ex
 
 SCRIPTDIR=$(cd $(dirname "$0") && pwd)
 ROOTDIR="$SCRIPTDIR/../.."
-WHISKDIR="$ROOTDIR/../openwhisk"
-
-export OPENWHISK_HOME=$WHISKDIR
 
 cd ${ROOTDIR}
-if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
-  TERM=dumb ./gradlew :tests:test
-else
-  TERM=dumb ./gradlew :tests:testWithoutCredentials
-fi
+TERM=dumb ./gradlew :tests:test
+
